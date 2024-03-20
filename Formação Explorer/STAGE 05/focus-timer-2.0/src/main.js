e3e5b3ed4b7746400699fcbcb63ff49398aebcd6
Bruno Lindoso <48108317+bslindoso@users.noreply.github.com>
2024-03-20 20:53:11 -0300

    focus-timer 2.0 html and css complete

--- Formação Explorer/STAGE 05/focus-timer-2.0/src/main.js
+++ Formação Explorer/STAGE 05/focus-timer-2.0/src/main.js
@@ -1,1 +1,21 @@
 import './toggle-mode.js'
+
+// TESTA A MUDANÇA ACTIVE
+const themes = document.querySelector('#themes')
+console.log(themes)
+
+themes.addEventListener('click', (event) => {
+  const activeButton = document.querySelector('#themes .active')
+  if (activeButton) {
+    activeButton.classList.toggle('active')
+  }
+
+  const sameButtonClick = activeButton != event.target.closest('button')
+  if (sameButtonClick) {
+    event.target.closest('button').classList.toggle('active')
+  }
+
+  // continue logic
+
+
+})
