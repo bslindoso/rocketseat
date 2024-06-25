feca9cf2f44e3c43725a1fba8860237aec958a73
Bruno Lindoso <48108317+bslindoso@users.noreply.github.com>
2024-06-25 14:42:02 -0300

    25-06-2024

--- Formação Nodejs/01-fundamentos/01-fundamentos-nodejs/src/server.js
+++ Formação Nodejs/01-fundamentos/01-fundamentos-nodejs/src/server.js
@@ -1,6 +1,27 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify({
+        type: "success",
+        data: users
+      }))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@example.com"
+    })
+    return res.end(`Criação de usuário\n${users[0].name} criado com sucesso`)
+  }
   return res.end("Olá Mundo")
 })
 
